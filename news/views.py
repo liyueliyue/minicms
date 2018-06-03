4718cc7d2a9d60f04b3b641b88bb2a03caf6607a
liyueliyue <1048355336@qq.com>
2018-06-03 21:10:20 +0800

    第三次提交

--- news/views.py
+++ news/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Column,Article
 
 # Create your views here.
 def index(request):
-    return HttpResponse("你好，这个是字符串！")
+    columns = Column.objects.all()
+    return render(request,'index.html',{'columns':columns})
 def column_detail(request,column_slug):
-    return HttpResponse('column slug:'+column_slug)
+    column = Column.objects.get(slug=column_slug)
+    return render(request,'news/column.html',{'column':column})
 def article_detail(request,article_slug):
-    return HttpResponse('article slug:'+article_slug)
+    article = Article.objects.filter(slug=article_slug)[0]
+    return render(request,'news/article.html',{'article':article})
